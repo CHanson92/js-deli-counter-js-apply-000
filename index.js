b901cfdd97d80744f7845564512b8519b784a3ad
Chris Hanson <blondyhanson@gmail.com>
2018-09-04 13:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function currentLine(line) {
   if (!line.length) {
     return "The line is currently empty."
@@ -19,9 +20,36 @@ function nowServing(line) {
 
   return `Currently serving ${line.shift()}.`
 };
+=======
+function CurrentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty, time to relax!"
+  }
+}
+
+  const NamesandDigits = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    NamesandDigits.push(`${i + 1}. ${line[i]}`)
+    return `The line is currently: ${numbersAndNames.join(', ')}`
+  }
+  
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served, time to relax!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+}
+>>>>>>> 85e45764d9bbac97012ffe4e110d46662d88f633
 
 function takeANumber(line, name) {
   line.push(name)
 
+<<<<<<< HEAD
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
+=======
+  return `Welcome, ${name}. You are number ${line.length} in line, take a breather before we serve you.`
+}
+>>>>>>> 85e45764d9bbac97012ffe4e110d46662d88f633
